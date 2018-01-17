be48da2827234d6e87409901f859b056b8cc3791
CainYang <flybirdyang@outlook.com>
2018-01-17 09:32:04 +0800

    Create 6ms.c

---
+++ Fizz Buzz/6ms.c
@@ -0,0 +1,33 @@
+/**
+ * Return an array of size *returnSize.
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+char** fizzBuzz(int n, int* returnSize) {
+    char** pString=(char**)malloc(sizeof(char*)*10000);
+    int i;
+    for(i=1;i<=n;i++)
+    {
+        if(i%3==0 && i%5==0)
+        {
+            pString[i-1] = (char*)malloc(sizeof(char)*10);
+            pString[i-1] = "FizzBuzz";
+        }
+        else if(i%3==0 && i%5!=0)
+        {
+            pString[i-1] = (char*)malloc(sizeof(char)*10);
+            pString[i-1] = "Fizz";
+        }
+        else if(i%3!=0 && i%5==0)
+        {
+            pString[i-1] = (char*)malloc(sizeof(char)*10);
+            pString[i-1] = "Buzz";
+        }
+        else
+        {
+            pString[i-1] = (char*)malloc(sizeof(char)*10);
+            sprintf(pString[i-1],"%d",i);
+        }
+    }
+    *returnSize = n;
+    return pString;
+}
